d8605c76c29d5bec76d6cbe1d7161bb99076d7a9
Frank Moda <Cicucci@Cicucci-Laptop.local>
2012-07-14 11:07:03 -0400

    Tanks now move around the board

--- client/js/client.js
+++ client/js/client.js
@@ -5,15 +5,29 @@ $(function () {
     var input = $('#input');
     var status = $('#status');
     
+    var grid_canvas = document.getElementById("game");
+    var grid = grid_canvas.getContext("2d");
+    
+    var board = new Object();
+    
+    var players = new Object();
+    
+    var myId = -1;
+    var myColor = "";
+    
+    var squareSize = 10;
+    
+    var started = false;
+    
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var connection = new WebSocket('ws://172.17.2.241:1337');
 
     connection.onopen = function () {
         // connection is opened and ready to use
         input.removeAttr('disabled');
-        status.text('Enter text:');
+        status.text('Enter Name:');
     };
 
     connection.onerror = function (error) {
@@ -31,7 +45,67 @@ $(function () {
         }
         // handle incoming message
         
-        if (json.type === 'messages') { // entire message history
+        if (json.type === 'connectsuccess') {
+            // Grab my id
+            myId = json.yourid;
+            
+            // Setup board
+            board = json.board;
+            grid_canvas.width = board.width;
+            grid_canvas.height = board.height;
+            grid.clearRect(0, 0, board.width, board.height);
+            grid.fillStyle = board['background-color'];
+            grid.fillRect(0, 0, board.width, board.height);
+            status.text('Board initialized');
+            
+            /*
+            // set the scene size
+            var WIDTH = 400,
+                HEIGHT = 300;
+
+            // set some camera attributes
+            var VIEW_ANGLE = 45,
+                ASPECT = WIDTH / HEIGHT,
+                NEAR = 0.1,
+                FAR = 10000;
+
+            // get the DOM element to attach to
+            // - assume we've got jQuery to hand
+            var $container = $('#game');
+
+            // create a WebGL renderer, camera
+            // and a scene
+            var renderer = new THREE.WebGLRenderer();
+            var camera = new THREE.PerspectiveCamera(
+                               VIEW_ANGLE,
+                               ASPECT,
+                               NEAR,
+                               FAR );
+
+            var scene = new THREE.Scene();
+
+            // the camera starts at 0,0,0 so pull it back
+            camera.position.z = 300;
+
+            // start the renderer
+            renderer.setSize(WIDTH, HEIGHT);
+
+            // attach the render-supplied DOM element
+            $container.append(renderer.domElement); */
+        } else if (json.type === 'update') {
+            // Add players
+            players = json.players;
+            for (var i=0; i < players.length; i++) {
+                if (players[i].id == myId) {
+                    myColor = players[i].color;
+                }
+                grid.fillStyle = players[i].color;
+                var position = players[i].position;
+                grid.fillRect(position.x-(squareSize/2),position.y-(squareSize/2),squareSize,squareSize);
+            }
+            status.text('Players initialized');
+            started = true;
+        } else if (json.type === 'messages') { // entire message history
             // insert every single message to the chat window
             for (var i=0; i < json.data.length; i++) {
                 addMessage(json.data[i].author, json.data[i].text, new Date(json.data[i].time));
@@ -57,6 +131,32 @@ $(function () {
         }
     });
     
+    $(document).keydown(function(e) {
+        if (started === true) {
+            if (e.keyCode === 38) {
+                // UP
+                var position = players[myId].position;
+                updatePlayerPosition(myId, position.x, position.y-5);
+                sendPosition();
+            } else if (e.keyCode === 40) {
+                // DOWN
+                var position = players[myId].position;
+                updatePlayerPosition(myId, position.x, position.y+5);
+                sendPosition();
+            } else if (e.keyCode === 37) {
+                // LEFT
+                var position = players[myId].position;
+                updatePlayerPosition(myId, position.x-5, position.y);
+                sendPosition();
+            } else if (e.keyCode === 39) {
+                // RIGHT
+                var position = players[myId].position;
+                updatePlayerPosition(myId, position.x+5, position.y);
+                sendPosition();
+            }
+        }
+    });
+    
     setInterval(function() {
         if (connection.readyState !== 1) {
             status.text('Error');
@@ -70,5 +170,21 @@ $(function () {
              + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
              + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
              + ': ' + message + '</p>');
+    }
+    
+    function sendPosition() {
+        var jsonmsg = JSON.stringify( { type : 'move', id : myId, 'new-pos' : { 'x' : players[id].position.x, 'y' : players[id].position.y, 'o' : players[id].position.o } } );
+        connection.send(jsonmsg);
+    }
+    
+    function updatePlayerPosition(id, x, y) {
+        var oldPosition = players[id].position;
+        grid.clearRect(oldPosition.x-(squareSize/2),oldPosition.y-(squareSize/2),squareSize,squareSize);
+        grid.fillStyle = board['background-color'];
+        grid.fillRect(oldPosition.x-(squareSize/2),oldPosition.y-(squareSize/2),squareSize,squareSize);
+        grid.fillStyle = players[id].color;
+        grid.fillRect(x-(squareSize/2),y-(squareSize/2),squareSize,squareSize);
+        players[id].position.x = x;
+        players[id].position.y = y;
     }
 });
