f2963c1c1b108f4dcead0beac99821d4f044aebc
mrwillis21 <mrwillis21@gmail.com>
2012-07-14 11:06:06 -0400

    Added connection response and board state update objects & responses.

--- server/js/server.js
+++ server/js/server.js
@@ -3,8 +3,46 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
-var messages = []
-var clients = []
+var nextId = -1;
+var clients = [];
+
+var boardWidth = 500;
+var boardHeight = 500;
+var boardColor = "#FFFFFF";
+var maxPlayers = 10;
+
+var boardState = new BoardState();
+
+function BoardInitializer(width, height, backgroundColor, maxPlayers) {
+    this.width = width,
+    this.height = height,
+    this["background-color"] = backgroundColor,
+    this["max-players"] = maxPlayers
+}
+
+function Connector(id, boardInitializer) {
+    this.type = "connectsuccess",
+    this.yourid = id,
+    this.board = boardInitializer
+}
+
+function Player(id, color, position, orientation) {
+    this.id = id,
+    this.color = color,
+    this.position = position,
+    this.orientation = orientation
+}
+
+function Position(x, y, o) {
+    this.x = x,
+    this.y = y,
+    this.o = o
+}
+
+function BoardState() {
+    this.type = "update",
+    this.players = []
+}
 
 /**
  * Helper function for escaping input strings
@@ -31,18 +69,24 @@ wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
     
     var index = clients.push(connection) - 1;
-    var userName = false;
+
+    var player = new Player(index, _getRandomColor(), _getRandomPosition(), _getRandomOrientation());
+    boardState.players.push(player);
+
+    var boardInit = new BoardInitializer(boardWidth, boardHeight, boardColor, maxPlayers);
+    var connectionData = new Connector(index, boardInit);
     
-    // send back chat history
-    if (messages.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'messages', data: messages} ));
-    }
+    // Send initializion message.
+    connection.sendUTF(JSON.stringify( connectionData ));
+
+    // Update board state for all players.
+    _updateBoard();
 
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            userName = request.origin;
+            /*userName = request.origin;
             // process WebSocket message
             var obj = {
                 time: (new Date()).getTime(),
@@ -55,15 +99,37 @@ wsServer.on('request', function(request) {
             var json = JSON.stringify({ type: 'message', data: obj });
             for (var i=0; i < clients.length; i++) {
                 clients[i].sendUTF(json);
-            }
+            }*/
         }
     });
 
     connection.on('close', function(connection) {
-        // close user connection
-        if (userName !== false) {
             // remove user from the list of connected clients
-            clients.splice(index, 1);
-        }
+            clients[index] = null;
     });
+
+    function _updateBoard() {    
+        for(var i = 0; i < clients.length; ++i) {
+            if(clients[i]) {
+                clients[i].sendUTF(JSON.stringify( boardState ));
+            }
+        }
+    }
+
+    function _getRandomPosition() {
+        // Change 200 to boardLeft + 1/2 of tank size
+        var x = Math.floor(Math.random()*200) + 200;
+        var y = Math.floor(Math.random()*200) + 200;
+        var o = _getRandomOrientation();
+        return new Position(x, y, o);
+    }
+
+    function _getRandomColor() {
+        return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+    }
+
+    function _getRandomOrientation() {
+        var orientations = ["U", "D", "L", "R"];
+        return orientations[Math.floor(Math.random()*5)];
+    }
 });
