f95626a4c1c2f2770e43c404d996ff01121adde4
Cicucci <fmoda3@Cicucci.local>
2012-07-13 23:44:15 -0400

    Skeletoned out some messaging for chat, but doesn't work on my machine...

--- client/js/client.js
+++ client/js/client.js
@@ -1,4 +1,10 @@
 $(function () {
+    "use strict";
+    
+    var game = $('#game');
+    var input = $('#input');
+    var status = $('#status');
+    
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
@@ -6,10 +12,13 @@ $(function () {
 
     connection.onopen = function () {
         // connection is opened and ready to use
+        input.removeAttr('disabled');
+        status.text('Enter text:');
     };
 
     connection.onerror = function (error) {
         // an error occurred when sending/receiving data
+        status.text('Error');
     };
 
     connection.onmessage = function (message) {
@@ -21,5 +30,45 @@ $(function () {
             return;
         }
         // handle incoming message
+        
+        if (json.type === 'messages') { // entire message history
+            // insert every single message to the chat window
+            for (var i=0; i < json.data.length; i++) {
+                addMessage(json.data[i].author, json.data[i].text, new Date(json.data[i].time));
+            }
+        } else if (json.type === 'message') { // it's a single message
+            input.removeAttr('disabled'); // let the user write another message
+            addMessage(json.data.author, json.data.text, new Date(json.data.time));
+        }
     };
+    
+    input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            var msg = $(this).val();
+            if (!msg) {
+                return;
+            }
+            // send the message as an ordinary text
+            connection.send(msg);
+            $(this).val('');
+            // disable the input field to make the user wait until server
+            // sends back response
+            input.attr('disabled', 'disabled');
+        }
+    });
+    
+    setInterval(function() {
+        if (connection.readyState !== 1) {
+            status.text('Error');
+            input.attr('disabled', 'disabled').val('Unable to comminucate '
+                                                             + 'with the WebSocket server.');
+        }
+    }, 3000);
+    
+    function addMessage(author, message, color, dt) {
+        game.append('<p><span>' + author + '</span> @ ' +
+             + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+             + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+             + ': ' + message + '</p>');
+    }
 });
--- server/js/server.js
+++ server/js/server.js
@@ -1,5 +1,18 @@
+"use strict";
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+
+var messages = []
+var clients = []
+
+/**
+ * Helper function for escaping input strings
+ */
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
 
 var server = http.createServer(function(request, response) {
     // process HTTP request. Since we're writing just WebSockets server
@@ -8,23 +21,49 @@ var server = http.createServer(function(request, response) {
 server.listen(1337, function() { });
 
 // create the server
-wsServer = new WebSocketServer({
+var wsServer = new WebSocketServer({
     httpServer: server
 });
 
 // WebSocket server
 wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
     var connection = request.accept(null, request.origin);
+    
+    var index = clients.push(connection) - 1;
+    var userName = false;
+    
+    // send back chat history
+    if (messages.length > 0) {
+        connection.sendUTF(JSON.stringify( { type: 'messages', data: messages} ));
+    }
 
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
+            userName = request.origin;
             // process WebSocket message
+            var obj = {
+                time: (new Date()).getTime(),
+                text: htmlEntities(message.utf8Data),
+                author: userName
+            }
+            messages.push(obj);
+            messages = messages.slice(-100);
+            
+            var json = JSON.stringify({ type: 'message', data: obj });
+            for (var i=0; i < clients.length; i++) {
+                clients[i].sendUTF(json);
+            }
         }
     });
 
     connection.on('close', function(connection) {
         // close user connection
+        if (userName !== false) {
+            // remove user from the list of connected clients
+            clients.splice(index, 1);
+        }
     });
 });
